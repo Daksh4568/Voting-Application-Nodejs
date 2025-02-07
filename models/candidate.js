f5dec74e12a6ccea5959db4950f5d0b198b7cdd1
Daksh Pratap Singh <daksh6666singh@gmail.com>
2025-02-07 13:07:04 +0530

    Created a voting application routes

---
+++ models/candidate.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+// Define the Person schema
+const candidateSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    party: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    votes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,// it is the mongoDB object id which mongodb create at the creation of any record . Its a unique id 
+                ref: 'User',
+                required: true
+            },
+            votedAt: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ],
+    voteCount: {
+        type: Number,
+        default: 0
+    }
+});
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+module.exports = Candidate;
